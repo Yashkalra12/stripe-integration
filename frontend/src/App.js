f4fc485057596bbdf4260145504ccdcfb42e97ce
Yashkalra12 <yash1578.be21@chitkara.edu.in>
2024-05-13 21:56:50 +0530

    stripe-key-fix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
+import React , {useCallback, useState} from 'react';
+import StripeCheckout from "react-stripe-checkout";
 
 function App() {
+
+  const [Product,setProduct]=useState({
+    name:"ProVersion",
+    price: 999,
+    productBy: "RecipeHub-Pro"
+
+  })
+
+
+  const makePayment= token =>{
+    const body={
+      token,
+      Product
+    }
+    const headers={
+      "Content-Type": "application/json"
+    }
+
+    return fetch(`http://localhost:8282/payment`, {
+      method:"POST",
+      headers,
+      body:JSON.stringify(body)
+
+    }).then(response =>{
+      console.log("RESPONSE", response);
+      const {status} =response;
+      console.log("STATUS", status);
+    })
+    .catch(err => console.log(err));
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="#"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
         </a>
+        <StripeCheckout 
+        stripeKey="pk_test_51PFx47SJRlGEPJpxwRwnGMCu04OoVz2TKHuxiYTwLGEhhZogxkA3EFzjwacyznS3r0nonLk4D2DXsbnptfUcOs2700ue2Zd3MW"
+        token={makePayment}
+        name="Get Started"
+        amount={Product.price*100}>
+          <button className='btn-large dark-yellow'>Get Started Buy ProVersion</button>  
+        </StripeCheckout>
       </header>
     </div>
   );
